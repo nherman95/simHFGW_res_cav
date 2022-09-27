0f0412d56faaeae940062093807060bc3180217a
Nicolas HERMAN <hermanicolas95@gmail.com>
2022-09-27 12:27:35 +0200

    nn

--- plot_mass.py
+++ plot_mass.py
@@ -10,7 +10,7 @@ rcParams['savefig.bbox'] = 'tight'
 rcParams['savefig.pad_inches'] = 0.1
 
 rcParams['figure.autolayout'] = False
-rcParams['figure.figsize'] = 15.5, 7
+rcParams['figure.figsize'] = 10,10
 rcParams['axes.labelsize'] = 28
 rcParams['axes.titlesize'] = 36
 rcParams['font.size'] = 16
@@ -19,24 +19,25 @@ rcParams['lines.markersize'] = 8
 rcParams['legend.fontsize'] = 14
 rcParams['xtick.labelsize'] = 22
 rcParams['ytick.labelsize'] = 22
+rcParams['text.usetex'] = True
 Mexp=loadtxt('mass.in')
-M=10**(-1*Mexp)
-Y=loadtxt('results/sim.txt')
+M=10**(-1*Mexp[6:])
+Y=loadtxt('simMass.txt')
 fig=plt.figure()
 ax=plt.gca()
 #Y=Y*4.640850738526967e-26*72511535.92617007*2*pi*5
-plt.loglog(M,Y[0,:],'b',linewidth=3)
+plt.loglog(M,Y[:,0],'b',)
 
-plt.loglog(M,Y[1,:],'b--')
-plt.loglog(M,Y[2,:],'b--')
-plt.fill_between(M,Y[1,:],Y[2,:],'b',alpha=0.35)
-plt.axis([1e-8,1e-2,1e-17,1e-9])
-xt=10.**arange(-8,-1)
+plt.loglog(M,Y[:,1],'r')
+plt.loglog(M,Y[:,2],'g')
+# plt.fill_between(M,Y[1,:],Y[2,:],'b',alpha=0.35)
+plt.axis([1e-8,1e-3,1e-17,1e-9])
+xt=10.**arange(-8,-2)
 print(xt)
 plt.xticks(xt)
 ax.set_box_aspect(1)
-plt.xlabel("Mass ($M_\odot$)")
-plt.ylabel("RMS Induced Power (W)")
-plt.title('TM Cavity',fontdict={'fontweight':'bold'})
-plt.savefig("masse.png")
-#plt.show()
+plt.xlabel(r"Mass ($M_\odot$)")
+plt.ylabel(r"RMS Induced Power (W)")
+plt.title(r'\textbf{TM Cavity}')
+plt.savefig("pbhmass.png")
+plt.show()
