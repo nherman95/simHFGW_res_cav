68f65353745a64e7fefb27c9bf5a460e0e6012c8
Nicolas HERMAN <hermanicolas95@gmail.com>
2022-10-18 11:39:48 +0200

    nnnn

--- plot_pbh.py
+++ plot_pbh.py
@@ -2,8 +2,9 @@ from cProfile import label
 from matplotlib import rcParams,ticker
 from matplotlib.pyplot import *
 from numpy import fft,pi
+from math import log
 import pickle
-from cavities import RMS_compute,fchirp
+from cavities import RMS_compute,fchirp, gw_binary_time_signal,gw_binary_freq_signal
 
 rcParams["font.family"] = "serif"
 rcParams["font.weight"] = 500
@@ -113,6 +114,7 @@ with open('./SIMFREQM3D25TM.pkl','rb') as f:  # Python 3: open(..., 'rb')
     f.close()
 
 figure(4)
+print(rmsP_t,rmsP_f,RMS_compute(fchirp,2200/25/M,2200/M,'TM',5,M))
 ax1=subplot(1,2,1)
 plot(t,dP_t,'b')
 axis([0,6e-3,-6e-12,6e-12])
@@ -140,6 +142,7 @@ with open('./SIMFREQM7D25TM.pkl','rb') as f:  # Python 3: open(..., 'rb')
     f.close()
 
 figure(5)
+print(rmsP_t,rmsP_f,RMS_compute(fchirp,2200/25/M,2200/M,'TEM',5,M))
 ax1=subplot(1,2,1)
 plot(t,dP_t,'b')
 axis([0,6e-7,-2e-11,2e-11])
@@ -161,3 +164,55 @@ savefig("pbh7TM.png")
 
 
 
+fig=figure(6)
+M=1e-5
+fisco=2200/M
+hp,hx,t=gw_binary_time_signal(M,1e9,fisco/25,1.0/2**(int(round(log(fisco,2)))+2))
+ax1=subplot(1,2,1)
+TFh_t = fft.rfft(hp,n=len(hp))*2/len(hp)
+freqdP_t = fft.rfftfreq(len(hp),t[1]-t[0])
+plot(t,hp,'b',label='Time domain code')
+#plot(t_f,dP_f,'g',alpha=0.6,label='Frequency domain code')
+axis([0,6e-5,-1.5e-28,1.5e-28])
+xlabel(r'Time(s)')
+ylabel(r'Strain')
+ax1.set_box_aspect(1)
+ax1.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True, useOffset=False))
+ax1.xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True, useOffset=False))
+ax2=subplot(1,2,2)
+#loglog(omega/2/pi,abs(TFdP_f)**2,'g')
+loglog(freqdP_t,abs(TFh_t)**2,'b')
+axis([1e4,1e9,1e-67,1e-59])
+xlabel(r'Frequency (Hz)')
+ylabel(r' $\left\vert \tilde{h} (\omega) \right\vert^2 $')
+ax2.set_box_aspect(1)
+#suptitle(r'\textbf{TM Cavity}')
+subplots_adjust(left=0.07,bottom=0.11,right=0.97,top=0.933,wspace=0.286,hspace=0.2)
+savefig("ht.png")
+
+fig=figure(7)
+M=1e-5
+fisco=2200/M
+TFhp,omega=gw_binary_freq_signal(M,1e9,fisco/25,fisco,1/t[-1])
+ax1=subplot(1,2,1)
+h = fft.irfft(TFhp,n=len(hp))*len(TFhp)
+plot(t,h,'b',label='Time domain code')
+#plot(t_f,dP_f,'g',alpha=0.6,label='Frequency domain code')
+axis([0,6e-5,-1.5e-28,1.5e-28])
+xlabel(r'Time(s)')
+ylabel(r'Strain')
+ax1.set_box_aspect(1)
+ax1.yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True, useOffset=False))
+ax1.xaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=True, useOffset=False))
+ax2=subplot(1,2,2)
+loglog(omega/2/pi,abs(TFhp)**2,'g')
+#loglog(freqdP_t,abs(TFh_t)**2,'b')
+axis([1e4,1e9,1e-67,1e-59])
+xlabel(r'Frequency (Hz)')
+ylabel(r' $\left\vert \tilde{h} (\omega) \right\vert^2 $')
+ax2.set_box_aspect(1)
+#suptitle(r'\textbf{TM Cavity}')
+subplots_adjust(left=0.07,bottom=0.11,right=0.97,top=0.933,wspace=0.286,hspace=0.2)
+savefig("hf.png")
+# handles,labels=ax1.get_legend_handles_labels()
+# fig.legend(handles,labels,loc='upper right')
